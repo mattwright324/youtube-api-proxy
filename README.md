326b8c43b21185958737ce86bc0d3e7284970ee3
Matthew Wright <mattwright324@gmail.com>
2024-12-26 14:30:23 -0500

    working v1 resolve_url and v3 request passthrough

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# youtube-api-proxy
+# YouTube API Proxy
+
+Self-host a user rate-limited YouTube API proxy (must provide your own key for v3).
+
+```
+GET https://ytapi.example.com/v1/resolve_url?url=https://www.youtube.com/@VanityUrl
+    -> POST https://youtubei.googleapis.com/youtubei/v1/navigation/resolve_url
+
+GET https://ytapi.example.com/v3/videos?part=snippet&id=dQw4w9WgXcQ 
+    -> GET https://www.googleapis.com/youtube/v3/videos?part=snippet&id=dQw4w9WgXcQ
+```
---
+++ server.mjs
@@ -0,0 +1,148 @@
+'use strict';
+
+import express from 'express';
+
+const app = express();
+import http from 'http';
+
+const server = http.createServer(app);
+import {dirname} from 'node:path';
+import {fileURLToPath} from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import cors from 'cors';
+app.use(cors({origin: process.env.CORS_WHITELIST.split(",")}));
+app.options('*', cors());
+app.set("trust proxy", 1);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+import {rateLimit} from 'express-rate-limit';
+
+const limiter = rateLimit({
+    windowMs: Number(process.env.LIMIT_WINDOW_MS),
+    limit: Number(process.env.LIMIT_COUNT),
+    standardHeaders: 'draft-8',
+    legacyHeaders: false,
+    message: "Exceeded usage limit, try again in a few minutes"
+});
+app.use(limiter);
+
+import NodeCache from "node-cache";
+
+const requestCache = new NodeCache({stdTTL: 300, checkperiod: 5})
+
+// import { Innertube } from "youtubei.js";
+// const innertube = await Innertube.create();
+
+const vanityRegexes = [
+    // Vanity @
+    /(?:http[s]?:\/\/)?(?:\w+\.)?youtube.com\/@([^\/?]+)(?:\?.*)?/i,
+    // Vanity user
+    /(?:http[s]?:\/\/)?(?:\w+\.)?youtube.com\/user\/([\w_-]+)(?:\?.*)?/i,
+    // Vanity custom
+    /(?:http[s]?:\/\/)?(?:\w+\.)?youtube.com\/c\/([^\/?]+)(?:\?.*)?/i,
+    /(?:http[s]?:\/\/)?(?:\w+\.)?youtube.com\/([^\/?]+)(?:\?.*)?/i
+]
+// Proxy for vanity url resolving
+app.get('/v1/resolve_url', async (req, res) => {
+    try {
+        const channelUrl = req.query.url;
+        if (!channelUrl) {
+            return res.status(400).send({message: '"url" query parameter is required'})
+        }
+
+        let matched = false
+        for (let i = 0; i < vanityRegexes.length; i++) {
+            const pattern = vanityRegexes[i];
+            const result = pattern.exec(channelUrl);
+            if (result) {
+                matched = true;
+            }
+        }
+        if (!matched) {
+            return res.status(400).send({message: '"url" is not an expected vanity url format'})
+        }
+
+        const CACHE_KEY = "/v1/resolve_url?url=" + channelUrl;
+        let cached = requestCache.get(CACHE_KEY);
+        if (cached) {
+            console.log("Cached", CACHE_KEY)
+            return res.status(cached.status).send(cached.json)
+        }
+
+        console.log("Request", CACHE_KEY)
+
+        const response = await fetch("https://youtubei.googleapis.com/youtubei/v1/navigation/resolve_url?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                context: {
+                    client: {
+                        clientName: "ANDROID",
+                        clientVersion: "19.09.37",
+                        hl: "en",
+                    }
+                },
+                "url": channelUrl
+            })
+        });
+        const responseJson = await response.json();
+
+        const isVanityUrl = responseJson?.endpoint?.commandMetadata?.resolveUrlCommandMetadata?.isVanityUrl;
+        const channelId = responseJson?.endpoint?.browseEndpoint?.browseId
+        const resultJson = {isVanityUrl: isVanityUrl, channelId: channelId};
+
+        requestCache.set(CACHE_KEY, {status: response.status, json: resultJson});
+        res.status(response.status).send(resultJson);
+    } catch (e) {
+        console.error(e)
+        res.status(500).send({message: 'A problem occurred'})
+    }
+});
+
+const allowed = {
+    paths: process.env.API_ALLOWED_PATHS.split(','),
+    params: process.env.API_ALLOWED_PARAMS.split(','),
+}
+// Proxy for GET requests
+app.get('/v3/*', async (req, res) => {
+    try {
+        const REQUEST_PATH = req.path.slice('/v3/'.length)
+        if (!allowed.paths.includes(REQUEST_PATH)) {
+            return res.status(403).send({message: 'Request path disallowed'})
+        }
+        for (const param in req.query) {
+            if (!allowed.params.includes(param)) {
+                delete req.query[param];
+            }
+        }
+        const REQUEST_PARAMS = Object.assign(req.query, {key: process.env.API_V3_KEY, quotaUser: req.ip})
+        const REQUEST_URL = "https://www.googleapis.com/youtube/v3/" + REQUEST_PATH + "?" + new URLSearchParams(REQUEST_PARAMS).toString()
+
+        let cached = requestCache.get(REQUEST_URL);
+        if (cached) {
+            console.log("Cached", REQUEST_URL)
+            return res.status(cached.status).send(cached.json)
+        }
+
+        console.log("Request", REQUEST_URL)
+
+        const response = await fetch(REQUEST_URL, {method: 'GET'});
+        const resultJson = await response.json();
+        requestCache.set(REQUEST_URL, {status: response.status, json: resultJson});
+        res.status(response.status).send(resultJson)
+    } catch (e) {
+        console.error(e)
+        res.status(500).send({message: 'A problem occurred'})
+    }
+});
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log('listening on *:3000');
+});
